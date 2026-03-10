26e0ebc4ae9b72a08eb732e0678b4b92d7956ac4
barrerosarah9 <barrerosarah9@gmail.com>
2026-03-10 12:11:35 -0500

    Eliminación de carpetas duplicadas
    Se eliminaron las carpetas duplicadas de Front-End y Back-End

--- Back/src/main/java/com/studyhub/repository/AsignaturaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studyhub.repository;
-
-import com.studyhub.model.Asignatura;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AsignaturaRepository extends JpaRepository<Asignatura, Long> {
-}
