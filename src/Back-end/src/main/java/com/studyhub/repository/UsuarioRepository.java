b46dfed675d3f9d0de1360f6f0919386689f2b04
valgomezzz <val.gomez.lm@gmail.com>
2026-03-18 22:28:11 -0500

    feat: Crear el repositorio de usuario. Closes #114

---
+++ src/Back-end/src/main/java/com/studyhub/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.studyhub.repository;
+
+import com.studyhub.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByCorreo(String correo);
+
+    boolean existsByCorreo(String correo);
+}
